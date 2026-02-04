8df30e7517185f3fde5593f82cbe921ed55154b6
KimA-Mik <ananim38@gmail.com>
2026-02-04 18:23:44 +0300

    Slap common util module with some useful stuff

---
+++ common/util/src/main/java/ru/kima/sonar/common/util/SonarResult.kt
@@ -0,0 +1,35 @@
+package ru.kima.sonar.common.util
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+
+sealed interface SonarResult<out S, out E> {
+    data class Success<out S, E>(val data: S) : SonarResult<S, E>
+    data class Error<S, out E>(val data: E) : SonarResult<S, E>
+}
+
+inline fun <S, E> SonarResult<S, E>.valueOr(alternative: (E) -> S): S {
+    return when (this) {
+        is SonarResult.Error -> alternative(data)
+        is SonarResult.Success -> data
+    }
+}
+
+@OptIn(ExperimentalContracts::class)
+fun <S, E> SonarResult<S, E>.isSuccess(): Boolean {
+    contract {
+        returns(true) implies (this@isSuccess is SonarResult.Success<S, E>)
+        returns(false) implies (this@isSuccess is SonarResult.Error<S, E>)
+    }
+    return this is SonarResult.Success<S, E>
+}
+
+@OptIn(ExperimentalContracts::class)
+fun <S, E> SonarResult<S, E>.isError(): Boolean {
+    contract {
+        returns(true) implies (this@isError is SonarResult.Error<S, E>)
+        returns(false) implies (this@isError is SonarResult.Success<S, E>)
+    }
+    return this is SonarResult.Error<S, E>
+}
