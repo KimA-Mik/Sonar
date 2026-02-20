2b7137e1c971220a04606eb9bebee64148f7effb
KimA-Mik <ananim38@gmail.com>
2026-02-20 19:13:46 +0300

    Slap dependencies

--- client/feature/portfolios/build.gradle.kts
+++ client/feature/portfolios/build.gradle.kts
@@ -1,15 +1,18 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.koin.compiler)
+    alias(libs.plugins.serialization)
 }
 
 android {
     namespace = "ru.kima.sonar.feature.portfolios"
     compileSdk {
-        version = release(36)
+        version = release(libs.versions.android.compileSdk.get().toInt())
     }
 
     defaultConfig {
-        minSdk = 24
+        minSdk = libs.versions.android.minSdk.get().toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -31,8 +34,24 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.koin.compose)
+
+    implementation(project(":client:common:ui"))
+    implementation(project(":client:data:home-api"))
+    implementation(project(":common:server-api"))
+    implementation(project(":common:util"))
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
