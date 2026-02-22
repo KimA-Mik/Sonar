5a6783c60b1e08b85852ba61a95febf16187291f
KimA-Mik <ananim38@gmail.com>
2026-02-22 23:53:59 +0300

    Add splash screen support and update token handling in MainActivity

--- client/app/src/main/java/ru/kima/sonar/MainActivity.kt
+++ client/app/src/main/java/ru/kima/sonar/MainActivity.kt
@@ -4,26 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import kotlinx.coroutines.flow.map
-import org.koin.android.ext.android.inject
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ru.kima.sonar.common.ui.theme.SonarTheme
-import ru.kima.sonar.data.applicationconfig.local.datasource.LocalConfigDataSource
 import ru.kima.sonar.ui.setEdgeToEdgeConfig
 
 
 class MainActivity : ComponentActivity() {
-    private val localConfig by inject<LocalConfigDataSource>()
-    private val tokenFlow = localConfig.localConfig().map { it.apiAccessToken }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setEdgeToEdgeConfig()
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { !SonarApplication.initialized }
         setContent {
             SonarTheme {
                 Surface {
-                    val token by tokenFlow.collectAsStateWithLifecycle(null)
-                    ApplicationScreen(authorised = token != null)
+                    val authorised by SonarApplication.loggedIn.collectAsState()
+                    ApplicationScreen(authorised = authorised)
                 }
             }
         }
