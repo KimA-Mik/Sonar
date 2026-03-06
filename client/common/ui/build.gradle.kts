faa9810b4d4a4cfda690c4022889173cb60ca482
KimA-Mik <ananim38@gmail.com>
2026-03-06 21:10:58 +0300

    Add menu

--- client/common/ui/build.gradle.kts
+++ client/common/ui/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.kotlinx.collections.immutable)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
